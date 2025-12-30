0532b8d3c8a73ce49779758101754ad5ba44cefd
Not Gustave <me@notgustave.com>
2025-12-30 14:03:33 +0700

    refactor: use static adapter

--- src/routes/docs/components/[...slug]/+page.ts
+++ src/routes/docs/components/[...slug]/+page.ts
@@ -3,12 +3,13 @@ import type { SvelteComponent } from 'svelte';
 import type { PageLoad } from './$types.js';
 
 export const load: PageLoad = async ({ params }) => {
+  const slug = params.slug.replace(/\/$/, '');
   const imports = import.meta.glob<{ default: SvelteComponent }>(
     '/src/lib/components/**/*.doc.svx'
   );
 
   for (const path in imports) {
-    if (path === `/src/lib/components/${params.slug}.doc.svx`) {
+    if (path === `/src/lib/components/${slug}.doc.svx`) {
       const doc = await imports[path]();
       return { component: doc.default };
     }
